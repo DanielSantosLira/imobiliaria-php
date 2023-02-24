483e27ebb0a70485748f4fafbc57ddfb24e604ce
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-23 22:29:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <link rel="shortcut icon" href="img/preview.jpg" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <title>Imobiliaria Luxurystate</title>
@@ -16,7 +17,7 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="img/preview.jpg" alt="" width="150px" height="75px"></a>
+                <a class="navbar-brand" href="#"><img src="img/preview.jpg" alt="" height="75px"></a>
 
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav justfy-content-center-end ">
@@ -47,19 +48,19 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/casa.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                    <img src="img/casa.jpg" class="d-block w-100" alt="..." width="800px" height="900px">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/casa-isolada.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                    <img src="img/casa-isolada.jpg" class="d-block w-100" alt="..." width="800px" height="900px">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/E2MStB2BK3UE.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                    <img src="img/E2MStB2BK3UE.jpg" class="d-block w-100" alt="..." width="800px" height="900px">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/jtHyeoDm0Cww.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                    <img src="img/jtHyeoDm0Cww.jpg" class="d-block w-100" alt="..." width="800px" height="900px">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/oJIrPmiAQY6j.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                    <img src="img/oJIrPmiAQY6j.jpg" class="d-block w-100" alt="..." width="800px" height="900px">
                 </div>
 
             </div>
@@ -73,70 +74,121 @@
             </button>
         </div>
     </section>
-    <div class="container mt-5">
+    <main class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu2NQ6f1i+hcEtu44Q6exvSPW5Z+FuI+QrUvO+Irwm46ExcaieO6E7tEu6ILMHV3wvv5OQvgtNBgkW9d+WhvlaNqJLmJ2jym0uRrUU7dqbHd241wRlE5tymumRp6Bxmcq4ia+VamI8Qy+YIXnAhliErMNAdsz6EIJ9xa9SUxy8h8d4563vSCAaKyyLIOvkiMYDwYu6oa6APevm5PPedV-GacxP3wP61NeW6wcmvuVAupw6QEZovrFTQeShQjQiOM3eYWrXNhLg-BVkwjDGbEYuE-ukoGV1ayuPE6CNVNu8r7N+PLiaPj7BanzOniih9tPtI3XPIgBZ4SjTyoHXWY1emZD9g==.jpg" class="card-img-top" alt="aqui vai uma foto">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">CASA DE PRAIA</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                        <a href="#" class="btn btn-success">ALUGA-SE</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu0tc+d1D4goygpMGYbSOlhKb5Z-N0P2WlEXH6brGpJzLxO3cBtf747gQ+IzYDgKjlpJYS4J3PEVUY-crfgLyedzbAHh68wewnFzXVqJ8RXV7-y5sjClSxXufe7Lz9wZtwQGpAtfB7x+9TpHMRGQFD8pHR9ox7ltMqBezU0J8p0oF7J6yoS7TO7S-K5+hxzlXFx5k9oa+SuKv15TFJsonDqooL34J41sRFqAe2beFX6J24QkLuOfUB1+EkBnHl+8kYMGwWtxOn7xQwQvEG7BNvEzqk9DC1vSlbR6FN1U9pr+ZqvKxb6r0EbTxJia8jp5But-IINkZf8ioCCQJEHlnYmhHuw0=.jpg" class="card-img-top" alt="aqui vai uma foto">
+                    <div class="card-body">
+                        <h5 class="card-title">CHÁCARA</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                        <a href="#" class="btn btn-success">VENDE-SE</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9bJDwnbk9Kawbnev1nxMNm9yHFhIP-MQkSx9WYNv8nxIz-NrwSwOo8gZR4OcuDKN+5TKys+Fs2anobzmgOrh2uHkAZ7b07AejPP3M1qv6YA6Xv0NPUJ-Zs9ZGiv3X4LZIU9q+GOIvk--X74bUXhx+1kOrDxXxAbnAMag828x23jgU8L7tiHTTonjC0MJCr9KAt8stFhX+AKpSRdyvwgS5oel4CDLfaapPtjgx3QYXVAy7sjlFbvklsaPPpplDa4jO3IPrFEfVe0TxraATORw8wkerqHXAwSWgwDDl+lqJpipXcEbyqcElw3ATeAfv0q+lNXOhvava0uGYFQ7p7ifq6a3NePyGrexem-uzNsT+Y6adYhVIMjqT2lVECwjcC8=.jpg" class="card-img-top" alt="aqui vai uma foto">
+                    <div class="card-body">
+                        <h5 class="card-title">CASA</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                        <a href="#" class="btn btn-success">ALUGA-SE</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu1Vj7YhPxAw3yJQ9TZahvgWo35qL7deojVqX2cyPhYSK+MjVU-rZ5awF-fbjAASV7oAgQ-YdKX5UdeJ7YwCvbb6yGkgB+h+HrWDISqNdXnJ1-Aczv0Zt+VjjYritqF403HuOAuz1jXOQSZjaWWNDQPlLANk18QdW9hinR0InpwcS45urs3PTcKG1MI36iGwAF0wy6oLwFffjnJmQZo1oFL4hPXoH4xwTV65f1uvISux44RMDv+uPFRGHlB-PhPAuPZ6tWN1Xz-UHkA-BGOQcu0vvw9aWjaf-bEmAMwA68+qf+KHlOvfvGKvvZGarwtVB5pLLbZAZK4-kQSRKQjQtdGIO.jpg" class="card-img-top" alt="aqui vai uma foto">
+                    <div class="card-body">
+                        <h5 class="card-title">APARTAMENTO</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                        <a href="#" class="btn btn-success">CASA-AVENDA</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-5">
             <div class="row">
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="img/pexels-leonard-dahmen.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu2NQ6f1i+hcEtu44Q6exvSPW5Z+FuI+QrUvO+Irwm46ExcaieO6E7tEu6ILMHV3wvv5OQvgtNBgkW9d+WhvlaNqJLmJ2jym0uRrUU7dqbHd241wRlE5tymumRp6Bxmcq4ia+VamI8Qy+YIXnAhliErMNAdsz6EIJ9xa9SUxy8h8d4563vSCAaKyyLIOvkiMYDwYu6oa6APevm5PPedV-GacxP3wP61NeW6wcmvuVAupw6QEZovrFTQeShQjQiOM3eYWrXNhLg-BVkwjDGbEYuE-ukoGV1ayuPE6CNVNu8r7N+PLiaPj7BanzOniih9tPtI3XPIgBZ4SjTyoHXWY1emZD9g==.jpg" class="card-img-top" alt="aqui vai uma foto">
                         <div class="card-body">
-                            <h5 class="card-title text-success">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-success">Go somewhere</a>
+                            <h5 class="card-title text-success">CASA DE PRAIA</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                            <a href="#" class="btn btn-success">ALUGA-SE</a>
                         </div>
                     </div>
 
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="img/pexels.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu0tc+d1D4goygpMGYbSOlhKb5Z-N0P2WlEXH6brGpJzLxO3cBtf747gQ+IzYDgKjlpJYS4J3PEVUY-crfgLyedzbAHh68wewnFzXVqJ8RXV7-y5sjClSxXufe7Lz9wZtwQGpAtfB7x+9TpHMRGQFD8pHR9ox7ltMqBezU0J8p0oF7J6yoS7TO7S-K5+hxzlXFx5k9oa+SuKv15TFJsonDqooL34J41sRFqAe2beFX6J24QkLuOfUB1+EkBnHl+8kYMGwWtxOn7xQwQvEG7BNvEzqk9DC1vSlbR6FN1U9pr+ZqvKxb6r0EbTxJia8jp5But-IINkZf8ioCCQJEHlnYmhHuw0=.jpg" class="card-img-top" alt="aqui vai uma foto">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">CHÁCARA</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                            <a href="#" class="btn btn-success">VENDE-SE</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="img/pexels-alina.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9bJDwnbk9Kawbnev1nxMNm9yHFhIP-MQkSx9WYNv8nxIz-NrwSwOo8gZR4OcuDKN+5TKys+Fs2anobzmgOrh2uHkAZ7b07AejPP3M1qv6YA6Xv0NPUJ-Zs9ZGiv3X4LZIU9q+GOIvk--X74bUXhx+1kOrDxXxAbnAMag828x23jgU8L7tiHTTonjC0MJCr9KAt8stFhX+AKpSRdyvwgS5oel4CDLfaapPtjgx3QYXVAy7sjlFbvklsaPPpplDa4jO3IPrFEfVe0TxraATORw8wkerqHXAwSWgwDDl+lqJpipXcEbyqcElw3ATeAfv0q+lNXOhvava0uGYFQ7p7ifq6a3NePyGrexem-uzNsT+Y6adYhVIMjqT2lVECwjcC8=.jpg" class="card-img-top" alt="aqui vai uma foto">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-secondary">Go somewhere</a>
+                            <h5 class="card-title">CASA</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                            <a href="#" class="btn btn-success">ALUGA-SE</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="img/pexels-alina.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <img src="https://imgs.kenlo.io/VWRCUkQ2Tnp3d1BJRDBJVe1szkhnWr9UfpZS9ftWwjXgr7v5Znen3XVcMHllDVRJJeIbi3YwVYEtu1Vj7YhPxAw3yJQ9TZahvgWo35qL7deojVqX2cyPhYSK+MjVU-rZ5awF-fbjAASV7oAgQ-YdKX5UdeJ7YwCvbb6yGkgB+h+HrWDISqNdXnJ1-Aczv0Zt+VjjYritqF403HuOAuz1jXOQSZjaWWNDQPlLANk18QdW9hinR0InpwcS45urs3PTcKG1MI36iGwAF0wy6oLwFffjnJmQZo1oFL4hPXoH4xwTV65f1uvISux44RMDv+uPFRGHlB-PhPAuPZ6tWN1Xz-UHkA-BGOQcu0vvw9aWjaf-bEmAMwA68+qf+KHlOvfvGKvvZGarwtVB5pLLbZAZK4-kQSRKQjQtdGIO.jpg" class="card-img-top" alt="aqui vai uma foto">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-secondary">Go somewhere</a>
+                            <h5 class="card-title">APARTAMENTO</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, esse enim neque officia eveniet non nostrum molestias, fugit laborum soluta temporibus quo repudiandae officiis velit, repellat itaque minus iure dolores.</p>
+                            <a href="#" class="btn btn-success">CASA-AVENDA</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </main>
+    <div class="container mt-5">
+        <form>
+
+            <div class="mb-3">
+                <h1 class="text-center">FORMULÁRIO</h1>
+                <label for="exampleInputEmail1" class="form-label">NOME </label>
+                <input type="email" class="form-control" id="nome">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">E-mail</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Cidade</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </form>
+    </div>
+    <div class="container mt-5">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.328661911507!2d-47.331282684888365!3d-22.753181138128557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677201622734!5m2!1spt-BR!2sbr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="text-center"></iframe>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
