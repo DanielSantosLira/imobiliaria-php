6d7d36a42fe3fe37cfd7f03b21bdab1f11030ab6
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-23 21:07:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,123 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
-    crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="shortcut icon" href="img/preview.jpg" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <title>Imobiliaria Luxurystate</title>
 </head>
+
 <body>
 
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="img/preview.jpg" alt="" width="150px" height="75px"></a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav justfy-content-center-end ">
+                        <li class="nav-item text-sucesso-ênfase">
+                            <a class="nav-link active " href="#">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">PRUDUTOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">CADASTRO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">CONTATOS</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section class="banner">
+        <div id="carouselExampleIndicators" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/casa.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/casa-isolada.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/E2MStB2BK3UE.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/jtHyeoDm0Cww.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/oJIrPmiAQY6j.jpg" class="d-block w-100" alt="..." width="650px" height="450px">
+                </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+    <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/pexels-leonard-dahmen.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-success">Go somewhere</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/pexels.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/pexels-alina.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-secondary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/pexels-alina.jpg" class="card-img-top" alt="aqui vai uma foto">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-secondary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
@@ -32,12 +138,11 @@
 
 
 
-
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
+
+
+</body>
+
 </html>
